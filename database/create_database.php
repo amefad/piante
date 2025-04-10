50fe0c862ed29cb8c1bf41f5c2264b4b2ed35dc1
Matteo <72200721+Matteo-Spina@users.noreply.github.com>
2025-04-10 21:30:34 +0200

    fix: updates config path in create_database.php
    Corrects the require path to 'public/config.php' to match the actual
    file location

--- database/create_database.php
+++ database/create_database.php
@@ -1,8 +1,8 @@
 <?php // Creates the database
 try {
-    require '../public/api/config.php'; // use config in public folder
+    require __DIR__ . '/../public/config.php'; // use config in public folder
     $pdo = new PDO(DB_HOST, DB_USER, DB_PASSWORD);
-    $sql = file_get_contents('database.sql');
+    $sql = file_get_contents(__DIR__ . '/database.sql');
     $sql = str_replace('`piante`', DB_NAME, $sql); // use your db name defined in config
     $pdo->exec($sql);
     echo 'Complimenti!<br>Hai creato il database <code>' . DB_NAME . '</code>.';
