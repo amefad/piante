091907530887b958583395e2c11fe7c4e5b79dae
Michele Salvador <michelesalvador@iol.it>
2025-07-17 22:04:05 +0200

    Add 404 route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Register from "./Register";
 import Confirm from "./Confirm";
 import Login from "./Login";
 import MapPage from "./MapPage";
+import Page from "./Page";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
       <Route path={`${import.meta.env.BASE_URL}/confirm`} element={<Confirm />} />
       <Route path={`${import.meta.env.BASE_URL}/login`} element={<Login />} />
       <Route path={`${import.meta.env.BASE_URL}/map`} element={<MapPage />} />
+      <Route path="*" element={<Page title="Niente qui" />} />
     </Routes>
   );
 }
