c04db9eabf269c2d7be34a93c24279d5a77d32f5
Matteo <72200721+Matteo-Spina@users.noreply.github.com>
2025-08-01 19:03:30 +0200

    Add react-leaflet

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,9 +1,27 @@
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 import "./Map.scss";
+
+const position = [45.887222, 12.296944];
+const bounds = [
+  [45.866446, 12.333395],
+  [45.947274, 12.323518],
+  [45.860204, 12.326402],
+  [45.903145, 12.24726],
+];
 
 export default function Map() {
   return (
-    <div className="map">
-      <p>Questa sarà la mappa con le piante</p>
-    </div>
+    <MapContainer center={position} bounds={bounds} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br />
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }
