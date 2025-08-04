0b349d392b9f052b971e256507fad034f978b6a1
Michele Salvador <michelesalvador@iol.it>
2025-08-04 13:50:52 +0200

    Fix marker path

--- src/Map.jsx
+++ src/Map.jsx
@@ -2,7 +2,7 @@ import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "./Map.scss";
 
-const position = [45.887222, 12.296944];
+const position = [45.8869, 12.29733];
 const bounds = [
   [45.866446, 12.333395],
   [45.947274, 12.323518],
@@ -11,13 +11,17 @@ const bounds = [
 ];
 
 export default function Map() {
+  const marker = L.icon({
+    iconUrl: `${import.meta.env.BASE_URL}/markers/map-pin.svg`,
+    iconAnchor: [9, 9],
+  });
   return (
     <MapContainer center={position} bounds={bounds} zoom={13} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
+      <Marker position={position} icon={marker}>
         <Popup>
           A pretty CSS3 popup. <br />
         </Popup>
