fb99974277de3706393c10d1b6b90042c2a9209c
Michele Salvador <michelesalvador@iol.it>
2025-08-05 22:21:33 +0200

    Remove end

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,7 +5,7 @@ import "./Header.scss";
 export default function Header() {
   return (
     <header>
-      <Link className="logo" to="/" end>
+      <Link className="logo" to="/">
         <img src={`${import.meta.env.BASE_URL}/favicon.svg`} alt="Logo" />
         <h1>Mappa delle piante</h1>
       </Link>
