bae9d79fcc83189d4a21ceb7fd1fa95f42d2e2fa
Matteo <72200721+Matteo-Spina@users.noreply.github.com>
2025-08-21 18:34:56 +0200

    Review AuthContext.jsx
    - Format AuthContext file using 2 spaces
    - Reorganize code
    - Use React 19 syntax: drop Provider
    - Add REVIEW.md to .gitignore

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -1,46 +1,45 @@
-import { useState, useEffect, createContext, useContext, use } from "react";
+import { useState, useEffect, createContext, useContext } from "react";
 
-const AuthContext = createContext();
 const TOKEN_KEY = "authToken";
 const USER_KEY = "userData";
 
-export const AuthProvider = ({ children }) => {
-    const [token, setToken] = useState(null);
-    const [user, setUser] = useState(null);
+const AuthContext = createContext(null);
 
-    function login({token, user}) {
-        localStorage.setItem(TOKEN_KEY, token);
-        localStorage.setItem(USER_KEY, JSON.stringify(user));
-        setToken(token);
-        setUser(user);
-    }
-    function logout() {
-        localStorage.removeItem(TOKEN_KEY);
-        localStorage.removeItem(USER_KEY);
-        setToken(null);
-        setUser(null);
-    }
-
-    useEffect(() => {
-        const storedToken = localStorage.getItem(TOKEN_KEY);
-        const storedUser = localStorage.getItem(USER_KEY);
-        if (storedToken && storedUser) {
-            setToken(storedToken);
-            setUser(JSON.parse(storedUser));
-        }
-    }, []);
-
-    return (
-        <AuthContext.Provider value={{ token, user, login, logout }}>
-            {children}
-        </AuthContext.Provider>
-    );
-}
-
+// eslint-disable-next-line react-refresh/only-export-components
 export const useAuth = () => {
-    const context = useContext(AuthContext);
-    if (!context) {
-        throw new Error("useAuth must be used within an AuthProvider");
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
+
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(null);
+  const [user, setUser] = useState(null);
+
+  function login({ token, user }) {
+    localStorage.setItem(TOKEN_KEY, token);
+    localStorage.setItem(USER_KEY, JSON.stringify(user));
+    setToken(token);
+    setUser(user);
+  }
+
+  function logout() {
+    localStorage.removeItem(TOKEN_KEY);
+    localStorage.removeItem(USER_KEY);
+    setToken(null);
+    setUser(null);
+  }
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem(TOKEN_KEY);
+    const storedUser = localStorage.getItem(USER_KEY);
+    if (storedToken && storedUser) {
+      setToken(storedToken);
+      setUser(JSON.parse(storedUser));
     }
-    return context;
-}
+  }, []);
+
+  return <AuthContext value={{ token, user, login, logout }}>{children}</AuthContext>;
+};
