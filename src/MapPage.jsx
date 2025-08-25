c43fbcd64f4b8f81a0b597cef89e007ebff5fe5a
Matteo <72200721+Matteo-Spina@users.noreply.github.com>
2025-08-25 19:10:54 +0200

    Draft retrieval of some data (WIP)

--- src/MapPage.jsx
+++ src/MapPage.jsx
@@ -1,11 +1,16 @@
 import Map from "./Map";
 import Access from "./Access";
+import { useData } from "./hooks/useData";
+
 import "./MapPage.scss";
 
 export default function MapPage() {
+  const { plants, isLoading, isError } = useData();
+  console.table(plants);
+  console.log(`MapPage -- isLoading ${isLoading}, isError ${isError}`);
   return (
     <div id="map-page">
-      <Map active={true} />
+      <Map active={true} plants={plants} />
       <Access />
     </div>
   );
