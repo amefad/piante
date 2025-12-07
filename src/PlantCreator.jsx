8fdfc36d3140aff02bf42585ac4afe1035408665
Michele Salvador <michelesalvador@iol.it>
2025-12-07 23:51:55 +0100

    Create Trunks component

--- src/PlantCreator.jsx
+++ src/PlantCreator.jsx
@@ -1,33 +1,40 @@
-import { useState, useContext } from "react";
+import { useState } from "react";
 import { useAuth } from "./AuthContext";
 import { useMapContext } from "./MapContext";
 import Autocomplete from "./Autocomplete";
+import Trunks from "./Trunks";
 import { disableMap, enableMap, resizeMap } from "./libs/map";
 import "./PlantCreator.scss";
 
 export default function PlantCreator() {
-  const { user } = useAuth();
+  const { user, token } = useAuth();
   const mapState = useMapContext();
   const [species, setSpecies] = useState({ id: 1 });
   const [number, setNumber] = useState("");
+  const [diameters, setDiameters] = useState([]);
   const [height, setHeight] = useState("");
   const [error, setError] = useState(null);
 
+  function gotoStep(step) {
+    mapState.setStep(step);
+    step == 2 ? disableMap(mapState.map) : enableMap(mapState.map);
+    resizeMap(mapState.map);
+  }
+
   // Posts the plant data to database
-  function addPlant() {
-    const token = localStorage.getItem("authToken");
+  function addPlant(event) {
+    event.preventDefault();
     const jsonData = {
-      number: number || null,
       latitude: mapState.plantLocation[0],
       longitude: mapState.plantLocation[1],
-      circumferences: [50], // TODO
+      number: number || null,
+      circumferences: diameters.length > 0 ? diameters : null,
       height: height || null,
       species: {
         id: species.id,
       },
       userId: user.id,
     };
-    console.table(jsonData);
     fetch(`${import.meta.env.BASE_URL}/api/plants`, {
       method: "POST",
       headers: { Authorization: `Bearer ${token}`, "Content-Type": "application/json" },
@@ -48,70 +55,53 @@ export default function PlantCreator() {
   if (user) {
     if (mapState.step == 0) {
       return (
-        <button
-          className="add"
-          onClick={() => {
-            mapState.setStep(1);
-            resizeMap(mapState.map);
-          }}
-          title="Aggiungi una pianta"
-        >
+        <button className="add" onClick={() => gotoStep(1)} title="Aggiungi una pianta">
           <img src={`${import.meta.env.BASE_URL}/img/plus.svg`} alt="+" />
         </button>
       );
     } else if (mapState.step == 1) {
       return (
-        <>
-          <section className="panel">
-            Posiziona la mappa e prosegui.
-            <button
-              onClick={() => {
-                mapState.setStep(2);
-                disableMap(mapState.map);
-                resizeMap(mapState.map);
-              }}
-              title="Passo successivo"
-            >
+        <section className="panel centered">
+          Posiziona la mappa e prosegui.
+          <div className="buttons">
+            <button type="button" onClick={() => gotoStep(0)} title="Annulla l'inserimento">
+              Annulla
+            </button>
+            <button onClick={() => gotoStep(2)} title="Passo successivo">
               Prosegui
             </button>
-          </section>
-        </>
+          </div>
+        </section>
       );
     } else {
       return (
-        <section className="panel">
+        <form className="panel" onSubmit={addPlant}>
           <Autocomplete value={"?"} setSpecies={setSpecies} />
           {species.warning && <mark>{species.warning}</mark>}
           <input
             type="number"
             placeholder="Numero comunale"
             value={number}
-            onChange={(event) => setNumber(event.target.value)}
+            onChange={(event) => setNumber(parseInt(event.target.value))}
           />
+          <Trunks setDiameters={setDiameters} />
           <input
             type="number"
             placeholder="Altezza (metri)"
             value={height}
-            onChange={(event) => setHeight(event.target.value)}
+            onChange={(event) => setHeight(parseFloat(event.target.value))}
           />
           <textarea placeholder="Note" value="Note (da implementare)" disabled />
           <div className="buttons">
-            <button
-              onClick={() => {
-                mapState.setStep(0);
-                enableMap(mapState.map);
-                resizeMap(mapState.map);
-              }}
-              title="Annulla l'inserimento"
-            >
-              Annulla
+            <button type="button" onClick={() => gotoStep(1)} title="Torna al posizionamento">
+              Indietro
             </button>
-            <button onClick={addPlant} title="Concludi l'inserimento">
+            <button type="submit" onClick={addPlant} title="Concludi l'inserimento">
               Salva
             </button>
           </div>
           {error && <p className="error">{error}</p>}
-        </section>
+        </form>
       );
     }
   }
---
+++ src/Trunks.jsx
@@ -0,0 +1,77 @@
+import { useState, useEffect } from "react";
+import "./Trunks.scss";
+
+export default function Trunks({ setDiameters }) {
+  const [method, setMethod] = useState("diameter");
+  const [measures, setMeasures] = useState([""]);
+
+  function addMeasure() {
+    setMeasures([...measures.slice(), ""]);
+  }
+
+  function removeMeasure() {
+    setMeasures(measures.slice(0, -1));
+  }
+
+  function changeMeasure(event, index) {
+    let value = event.target.value;
+    if (value <= 0) {
+      value = "";
+    }
+    setMeasures(measures.map((measure, i) => (i == index ? value : measure)));
+  }
+
+  // Sets data that will go to database
+  useEffect(() => {
+    if (method == "none") {
+      setDiameters(["unable"]);
+    } else {
+      const diameters = measures
+        .map((measure) => (method == "circum" ? Math.round(measure / Math.PI) : parseInt(measure)))
+        .filter((diameter) => diameter > 0)
+        .sort((a, b) => b - a);
+      setDiameters(diameters);
+    }
+  }, [method, measures]);
+
+  return (
+    <div id="trunks">
+      <div className="methods" onChange={(event) => setMethod(event.target.value)}>
+        <label>
+          <input
+            type="radio"
+            name="method"
+            value="diameter"
+            defaultChecked={method == "diameter"}
+          />
+          Diametri
+        </label>
+        <br />
+        <label>
+          <input type="radio" name="method" value="circum" defaultChecked={method == "circum"} />
+          Circonferenze
+        </label>
+        <br />
+        <label>
+          <input type="radio" name="method" value="none" defaultChecked={method == "none"} />
+          Non misurabile
+        </label>
+      </div>
+      {method != "none" && (
+        <div className="values">
+          {measures.map((measure, index) => (
+            <input
+              key={index}
+              type="number"
+              placeholder="Centimetri"
+              onChange={(event) => changeMeasure(event, index)}
+              value={measure}
+            />
+          ))}
+          {measures.length > 1 && <input type="button" value="-" onClick={removeMeasure} />}
+          <input type="button" value="+" onClick={addMeasure} />
+        </div>
+      )}
+    </div>
+  );
+}
